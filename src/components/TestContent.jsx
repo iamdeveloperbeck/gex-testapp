1c05bf63aa23e425b355fda849580662c33bd3e9
Developer Beck <85181067+iamdeveloperbeck@users.noreply.github.com>
2024-10-10 11:47:40 +0500

    Update TestContent.jsx

--- src/components/TestContent.jsx
+++ src/components/TestContent.jsx
@@ -182,7 +182,7 @@ const TestContent = () => {
               <button onClick={generatePDF} className='text-white bg-gradient-to-r from-blue-500 via-blue-600 to-blue-700 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2'>
                 Natijalarni yuklab olish
               </button>
-              <Link to='/'>Bosh saxifaga qaytish</Link>
+              <Link to='/' className="text-white bg-gradient-to-r from-green-400 via-green-500 to-green-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2">Bosh saxifaga qaytish</Link>
             </div>
           </div>
         </div>
