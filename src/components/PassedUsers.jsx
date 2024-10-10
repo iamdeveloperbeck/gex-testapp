ce819a26e6c1f166e3f25ba4028bb60ab32293b3
Developer Beck <85181067+iamdeveloperbeck@users.noreply.github.com>
2024-10-10 11:33:40 +0500

    Create PassedUsers.jsx

---
+++ src/components/PassedUsers.jsx
@@ -0,0 +1,18 @@
+
+
+const PassedUsers = () => {
+  const passedUsers = JSON.parse(localStorage.getItem('passedUsers')) || [];
+
+  return (
+    <div>
+      <h2>Passed Users</h2>
+      <ul>
+        {passedUsers.map((user, idx) => (
+          <li key={idx}>{`${user.name} ${user.surname} - Score: ${user.score}`}</li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default PassedUsers;
