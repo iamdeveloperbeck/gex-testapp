d39e7e9efda8b31ed5b19e7313f0fb6c69fdf879
Developer Beck <85181067+iamdeveloperbeck@users.noreply.github.com>
2024-10-10 11:38:47 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import Home from './components/Home';
 import Wrapper from './components/Wrapper';
 import TestContent from './components/TestContent';
+import PassedUsers from './components/PassedUsers';
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
         <Route path='/' element={<Home />} />
         <Route path='/wrap' element={<Wrapper />} />
         <Route path='/testcontent' element={<TestContent />} />
+        <Route path='/passedusers' element={<PassedUsers />} />
       </Routes>
     </>
   );
